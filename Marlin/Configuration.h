8771d424ffcd970ee17ba930e573bdc78e28a063
vert <vert@vert.com>
2021-01-17 20:16:33 -0500

    start B502

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -72,7 +72,7 @@
 // Author info of this build printed to the host during boot and M115
 #define STRING_CONFIG_H_AUTHOR "(Vertabreaker)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
-#define SHORT_BUILD_VERSION "Bugfix Build 501"
+#define SHORT_BUILD_VERSION "Bugfix Build 502"
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------------------------------------------
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-There is no difference with upstream besides configuration (configuration.h, configuration_adv.h).
+There is no difference between upstream marlin other then my configuration files (configuration.h, configuration_adv.h) 
+They where all chopped up in to seperate example files and merged in to source back in 2019.
+This repo is generally maintained for my personal use, as a legacy of times past. 
