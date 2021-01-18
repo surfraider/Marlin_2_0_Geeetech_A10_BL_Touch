9abe256183ecd45672209e551bed77b4daedae8a
vert <vert@vert.com>
2021-01-18 18:04:13 -0500

    clean up

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -107,9 +107,6 @@
   #if ENABLED (GTA10PRO)
     #define GTA10
     #define YHCB2004 // A10 Pro  Screen    
-    #define BTN_EN1          16 
-    #define BTN_EN2          17
-    #define BTN_ENC          19
   #endif
 
   #if ENABLED (GTA10T)
